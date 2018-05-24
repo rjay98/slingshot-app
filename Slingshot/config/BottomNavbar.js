f12f32ebf88d259a59849f9675275f816ba72a55
Ryan Jiang <ryan_jiang@college.harvard.edu>
2018-05-24 12:26:26 -0400

    added icons to navbar

--- Slingshot/config/BottomNavbar.js
+++ Slingshot/config/BottomNavbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { createBottomTabNavigator } from 'react-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
@@ -11,5 +11,25 @@ export default createBottomTabNavigator(
     Home: {screen: HomeScreen},
     Profile: {screen: ProfileScreen},
     Discover: {screen: DiscoverScreen}
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `home`;
+        } else if (routeName === 'Profile') {
+          iconName = `account-circle`;
+        } else if (routeName === 'Discover') {
+          iconName = `view-headline`;
+        }
+        return <Icon name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
   }
 )
